7ed931a4cf0f82c8a38900eb6143fa07a35f7e55
Obi Enechionyia <obienechionyia@gmail.com>
2022-12-18 03:34:24 +0100

    Update navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,38 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import './Nav.css';
-import Logo from './movie-logo.png';
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import Logo from "./movie-logo.png";
 import Menu from "./menu-icon.jpg";
-import { useGlobalContext } from './context';
+import { useGlobalContext } from "./context";
 
 function Nav() {
-    const { openSidebar, closeSidebar, isSidebarOpen } = useGlobalContext();
-   const [show, setShow] = useState(false);
-  
-   useEffect(() => {
-       window.addEventListener("scroll", () => {
-           if (window.scrollY > 100) {
-               setShow(true);
-           } else {
-               setShow(false);
-           }
-       });
-   }, []);
+  const { openSidebar, closeSidebar, isSidebarOpen } = useGlobalContext();
+  const [show, setShow] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+  }, []);
 
   return (
     <div className={show ? "nav-black" : "nav"}>
-        <img
-            className="nav-logo"
-            src={ Logo }
-            alt="Movie App Logo"
-        />
-        <img onClick={isSidebarOpen ? closeSidebar : openSidebar}
-            className={isSidebarOpen ? "nav-menu nav-open" : "nav-menu"}
-            src={ Menu }
-            alt="Menu Icon"
-        />
+      <h1 className="nav-title">Movie Finder</h1>
+      {/* <img className="nav-logo" src={Logo} alt="Movie App Logo" /> */}
+      <img
+        onClick={isSidebarOpen ? closeSidebar : openSidebar}
+        className={isSidebarOpen ? "nav-menu nav-open" : "nav-menu"}
+        src={Logo}
+        alt="Menu Icon"
+      />
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;

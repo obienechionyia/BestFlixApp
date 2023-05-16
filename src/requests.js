1dbcdc074ebc641b10d091fcd67ae28ac18bd4c0
Obi Enechionyia <obienechionyia@gmail.com>
2023-05-17 01:37:45 +0200

    Updates

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = "28441590cb486ac8b9fccd6ec31f9fbf";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
